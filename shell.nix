d6a87bd4db95d0fc592775104d1a4ea0e71e0314
willis plummer <willisplummer@gmail.com>
2019-10-08 01:58:40 -0400

    first pass at adding ghcide to shell

--- shell.nix
+++ shell.nix
@@ -1,1 +1,27 @@
-(import ./release.nix).project.env
+let
+  pkgs = import <nixpkgs> { };
+
+  ghcVersion = "ghc865";
+
+  haskellPackages = pkgs.haskell.packages.${ghcVersion}.override { 
+    overrides = self: super: {
+      # If you need a particular version of a package instead
+      # of the default that nixpkgs provides, specify it here.
+    };
+  };
+
+  ghcide =
+    let 
+      url = "https://github.com/hercules-ci/ghcide-nix/tarball/master";
+    in
+      (import (builtins.fetchTarball url) {})."ghcide-${ghcVersion}";
+
+
+  oldDerivation = (import ./release.nix).project.env;
+
+in pkgs.lib.overrideDerivation oldDerivation (old: {
+    buildInputs = old.buildInputs ++ [
+      haskellPackages.ghcid
+      ghcide
+    ];
+})
